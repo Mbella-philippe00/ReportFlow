ffd1dda8206d6de139949c3ae2da6194e90aa5fb
Mbella-philippe00 <mbellacriss@icloud.com>
2026-07-23 18:01:23 +0100

    Trust proxy headers on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,8 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(SecurityHeaders::class);
-    })
+    $middleware->trustProxies(
+        at: '*'
+    );
+
+    $middleware->append(SecurityHeaders::class);
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
