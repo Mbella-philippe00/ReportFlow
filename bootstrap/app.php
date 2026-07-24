2acefd0c794672929d6f94b318238bf05a018d2e
Mbella-philippe00 <mbellacriss@icloud.com>
2026-07-24 13:26:37 +0100

    Ignore forwarded internal port on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,14 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+   ->withMiddleware(function (Middleware $middleware): void {
     $middleware->trustProxies(
         at: '*',
         headers: Request::HEADER_X_FORWARDED_FOR
             | Request::HEADER_X_FORWARDED_HOST
-            | Request::HEADER_X_FORWARDED_PORT
             | Request::HEADER_X_FORWARDED_PROTO
-            | Request::HEADER_X_FORWARDED_PREFIX
     );
 
     $middleware->append(SecurityHeaders::class);
